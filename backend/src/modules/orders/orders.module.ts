0f4df14e92d35b1bd30c5b143aa4770da6c15801
FLYLIKEB <rerub0831@gmail.com>
2026-03-30 13:48:50 +0900

    fix: #25 #28 PointHistory 모듈 등록

--- backend/src/modules/orders/orders.module.ts
+++ backend/src/modules/orders/orders.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
+import { PointHistory } from '../coupons/entities/point-history.entity';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, PointHistory])],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
