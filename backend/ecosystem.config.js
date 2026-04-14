4dee3ef971f735dccaa78e06ceee184ffbe06c13
FLYLIKEB <rerub0831@gmail.com>
2026-04-14 23:09:08 +0900

    fix: PM2 환경에 JWT_PRIVATE_KEY_FILE 경로 추가

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,6 +11,7 @@ module.exports = {
       AWS_REGION: 'ap-northeast-2',
       AWS_S3_BUCKET_NAME: 'okhwadang-assets',
       AWS_CDN_URL: 'https://dt24i8idwxww1.cloudfront.net',
+      JWT_PRIVATE_KEY_FILE: '/app/shop-okhwadang/shop-okhwadang/backend/keys/jwt-private.pem',
     },
     max_memory_restart: '512M',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
